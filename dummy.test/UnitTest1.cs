26c3c844f94a84d1c8377fba9a7b1edfdff49641
Thomas Poulsen <Thomas.Poulsen@3shape.com>
2023-03-01 14:12:58 +0100

    something

--- dummy.test/UnitTest1.cs
+++ dummy.test/UnitTest1.cs
@@ -1,16 +1,27 @@
+using Xunit.Abstractions;
+
 namespace dummy.test
 {
     public class UnitTest1
     {
+        private readonly ITestOutputHelper output;
+
+        public UnitTest1(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
         [Fact]
         public void Test1()
         {
+            output.WriteLine("Hello from test 1");
             Assert.Equal(1, 1);
         }
 
         [Fact]
         public void Test2()
         {
+            output.WriteLine("Hello from test 2");
             Assert.Equal(1, 2);
         }
     }
