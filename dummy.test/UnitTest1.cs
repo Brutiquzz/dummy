2c160a707775ddcd90e90ab1b4800b59b4e9c1ac
Thomas Poulsen <Thomas.Poulsen@3shape.com>
2023-03-01 13:10:40 +0100

    Initial

---
+++ dummy.test/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace dummy.test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, 1);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.Equal(1, 2);
+        }
+    }
+}
